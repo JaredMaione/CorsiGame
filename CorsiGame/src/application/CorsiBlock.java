be44e04819d11de7767b98eb6bed06af779a0195
JaredMaione <jaredm866@gmail.com>
2019-09-20 15:32:14 -0600

    Added constructor, members, and methods

--- CorsiGame/src/application/CorsiBlock.java
+++ CorsiGame/src/application/CorsiBlock.java
@@ -1,5 +1,40 @@
 package application;
 
-public class CorsiBlock {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
+public class CorsiBlock extends Rectangle
+{
+	private final Color LIT_COLOR = Color.YELLOW;
+	private final Color UNLIT_COLOR = Color.BLUE;
+	
+	private boolean isLit;
+	private Stopwatch blinkStopwatch;
+	
+	public CorsiBlock()
+	{
+		isLit = false;
+		setFill(UNLIT_COLOR);
+		
+		blinkStopwatch = new Stopwatch();
+	}
+	
+	public void setLit(boolean lit)
+	{
+		isLit = lit;
+		
+		if (isLit)
+		{
+			setFill(LIT_COLOR);
+		}
+		else
+		{
+			setFill(UNLIT_COLOR);
+		}
+	}
+	
+	public boolean isLit()
+	{
+		return isLit;
+	}
 }
