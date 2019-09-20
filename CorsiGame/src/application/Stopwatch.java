78e973dca6adb0d679db961425691ea96f64c436
JaredMaione <jaredm866@gmail.com>
2019-09-20 15:41:55 -0600

    Added constructor and methods

--- CorsiGame/src/application/Stopwatch.java
+++ CorsiGame/src/application/Stopwatch.java
@@ -1,5 +1,37 @@
 package application;
 
-public class Stopwatch {
+public class Stopwatch 
+{
+	boolean isRunning;
+	
+	// In MS
+	int startTime;
+	int endTime;
+	
+	ElapsedTime elapsedTime;
+	
+	public Stopwatch()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
 
+	public ElapsedTime stop()
+	{
+		return elapsedTime;
+	}
+	
+	public ElapsedTime getLastElapsedTime()
+	{
+		return elapsedTime;
+	}
+	
+	public void reset()
+	{
+		
+	}
 }
