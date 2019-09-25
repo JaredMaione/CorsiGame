dbc7f3295bc9b8d8c9589b8e8dedbf9f35acc9f2
JaredMaione <jaredm866@gmail.com>
2019-09-24 19:19:15 -0600

    Added method generateBlocks(int, int, int)

--- CorsiGame/src/application/CorsiBlockGenerator.java
+++ CorsiGame/src/application/CorsiBlockGenerator.java
@@ -1,5 +1,20 @@
 package application;
 
-public class CorsiBlockGenerator {
+import java.util.ArrayList;
 
+public class CorsiBlockGenerator 
+{
+	private final int BLOCK_SIDE_LENGTH = 100;
+	
+	public ArrayList<CorsiBlock> generateBlocks(int numBlocks, int maxX, int maxY)
+	{
+		ArrayList<CorsiBlock> blocks = new ArrayList<CorsiBlock>();
+		
+		for (int i = 0; i < numBlocks; ++i) 
+		{
+			blocks.add(new CorsiBlock((Math.random() * (maxX - BLOCK_SIDE_LENGTH)), (Math.random() * (maxY - BLOCK_SIDE_LENGTH)), BLOCK_SIDE_LENGTH));
+		}
+		
+		return blocks;
+	}
 }
