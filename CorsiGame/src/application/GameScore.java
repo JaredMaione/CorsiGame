6130dbcf69d3ea4b0fbd0d80cfc68dd43d122acd
JaredMaione <jaredm866@gmail.com>
2019-09-27 17:30:09 -0600

    Began writing GameScore class

--- CorsiGame/src/application/GameScore.java
+++ CorsiGame/src/application/GameScore.java
@@ -1,5 +1,49 @@
 package application;
 
-public class GameScore {
+import java.util.ArrayList;
 
+public class GameScore 
+{
+	private int corsiSpan;
+	private ElapsedTime avgSequenceTime;
+	private ArrayList<ElapsedTime> sequenceTimes;
+	private ElapsedTime gameDuration;
+	
+	public GameScore()
+	{
+		corsiSpan = 0;
+	}
+	
+	public void addToAvgSequenceTime(ElapsedTime time)
+	{
+		
+	}
+	
+	private ElapsedTime calculateAverage()
+	{
+		if (sequenceTimes.size() == 0)
+		{
+			return new ElapsedTime(0);
+		}
+	}
+	
+	public int getCorsiSpan() 
+	{
+		return corsiSpan;
+	}
+	
+	public void setCorsiSpan(int span) 
+	{
+		corsiSpan = span;
+	}
+	
+	public ElapsedTime getGameDuration()
+	{
+		return gameDuration;
+	}
+	
+	public void setGameDuration(ElapsedTime duration)
+	{
+		gameDuration = duration;
+	}
 }
