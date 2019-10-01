14ef0c7c93e344ff1f4bb780b6350842298fdbd3
JaredMaione <jaredm866@gmail.com>
2019-10-01 12:31:24 -0600

    Added members

--- CorsiGame/src/application/PlayerData.java
+++ CorsiGame/src/application/PlayerData.java
@@ -1,5 +1,14 @@
 package application;
 
-public class PlayerData {
+import java.util.ArrayList;
 
+public class PlayerData 
+{
+	private int id;
+	private Date dob;
+	private String city;
+	private String state;
+	private String country;
+	private String diagnosis;
+	private ArrayList<GameScore> scores;
 }
