f578b6402938055b5de51f0fd42a66e22f406b50
JaredMaione <jaredm866@gmail.com>
2019-10-01 12:00:39 -0600

    Added class CorsiBlockClickHandler
    This class will handle when blocks are clicked

---
+++ CorsiGame/src/application/CorsiBlockClickHandler.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class CorsiBlockClickHandler implements EventHandler<MouseEvent>
+{
+
+	@Override
+	public void handle(MouseEvent e)
+	{
+
+	}
+
+	//radioButtonNo.addEventFilter(MouseEvent.MOUSE_CLICKED, noButtonHandler);
+}
