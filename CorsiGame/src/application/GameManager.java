6d20441e694d17f7ba1e42ab44c62d3366e6d61b
JaredMaione <jaredm866@gmail.com>
2019-10-08 09:26:50 -0600

    Added submit button to submit sequence for validation

--- CorsiGame/src/application/GameManager.java
+++ CorsiGame/src/application/GameManager.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 public class GameManager 
 {
+	private Button submitButton;
+	private final String SUBMIT_BUTTON_LABEL = "Submit";
+	
 	private ArrayList<CorsiBlock> blocks;
 	private ArrayList<CorsiBlock> sequence;
 	private PlayerData playerData;
@@ -23,12 +27,25 @@ public class GameManager
 	
 	private final int NUM_BLOCKS = 9;
 	
+	// This value will be subtracted from the height of the scene to locate submit button
+	private final int SCENE_Y_OFFSET = 50;
+	
 	public GameManager(PlayerData playerData, Group objectGroup)
 	{
 		this.playerData = playerData;
 		gameObjects = objectGroup;
-		blocks = CorsiBlockGenerator.generateBlocks(NUM_BLOCKS, (int) gameObjects.getScene().getWidth(), (int) gameObjects.getScene().getHeight());
+		
+		submitButton = new Button(SUBMIT_BUTTON_LABEL);
+		submitButton.setLayoutX(0);
+		submitButton.setLayoutY(gameObjects.getScene().getHeight() - (SCENE_Y_OFFSET - 15));
+		
+		gameObjects.getChildren().add(submitButton);
+		
+		blocks = CorsiBlockGenerator.generateBlocks(NUM_BLOCKS, (int) gameObjects.getScene().getWidth(), 
+																(int) (gameObjects.getScene().getHeight() - SCENE_Y_OFFSET));
 		currentLevel = 2;
+		
+
 		
 		sequencePlayer = new CorsiSequencePlayer();
 		sequenceTimer = new Stopwatch();
