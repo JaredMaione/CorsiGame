7dcad772ab08498b9ab668d16d388e079dd7c92e
JaredMaione <jaredm866@gmail.com>
2019-10-13 13:30:44 -0600

    Added menu classes

---
+++ CorsiGame/src/application/MainMenu.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.stage.Stage;
+
+public class MainMenu 
+{
+	public MainMenu(Stage stage)
+	{
+		
+	}
+}
---
+++ CorsiGame/src/application/RegistrationMenu.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.stage.Stage;
+
+public class RegistrationMenu 
+{
+	public RegistrationMenu(Stage stage)
+	{
+		
+	}
+}
