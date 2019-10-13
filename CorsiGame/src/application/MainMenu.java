beb7e6daec6b50e9d0fee59b52ff4b15770d61e4
JaredMaione <jaredm866@gmail.com>
2019-10-13 13:33:59 -0600

    Began adding fields

--- CorsiGame/src/application/MainMenu.java
+++ CorsiGame/src/application/MainMenu.java
@@ -1,9 +1,19 @@
 package application;
 
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class MainMenu 
 {
+	private Group group;
+	private HBox buttonBox;
+	
+	private Button newPlayerButton;
+	
+	private Stage stage;
+	
 	public MainMenu(Stage stage)
 	{
 		
