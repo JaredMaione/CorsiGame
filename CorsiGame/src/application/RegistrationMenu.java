a48a3e77d04c7673f405561a965d234effb531ef
JaredMaione <jaredm866@gmail.com>
2019-10-15 15:03:22 -0600

    Added semicolons to labels

--- CorsiGame/src/application/RegistrationMenu.java
+++ CorsiGame/src/application/RegistrationMenu.java
@@ -10,12 +10,12 @@ public class RegistrationMenu
 {
 	private TwoColumnPane formPane;
 	
-	private final String NAME_FIELD_LABEL = "Name";
-	private final String DOB_FIELD_LABEL = "Date of Birth";
-	private final String CITY_FIELD_LABEL = "City";
-	private final String STATE_FIELD_LABEL = "State, Province, or Region";
-	private final String COUNTRY_FIELD_LABEL = "Country";
-	private final String DIAGNOSIS_FIELD_LABEL = "Diagnosis";
+	private final String NAME_FIELD_LABEL = "Name:";
+	private final String DOB_FIELD_LABEL = "Date of Birth:";
+	private final String CITY_FIELD_LABEL = "City:";
+	private final String STATE_FIELD_LABEL = "State, Province, or Region:";
+	private final String COUNTRY_FIELD_LABEL = "Country:";
+	private final String DIAGNOSIS_FIELD_LABEL = "Diagnosis:";
 	
 	private TextField nameField;
 	private TextField dobField;
