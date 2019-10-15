4cd64c121416e10f90cfae1a2470629f4b41676c
JaredMaione <jaredm866@gmail.com>
2019-10-15 14:43:59 -0600

    Made changes regarding the addition of nodes

--- CorsiGame/src/application/TwoColumnPane.java
+++ CorsiGame/src/application/TwoColumnPane.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
@@ -7,6 +9,8 @@ import javafx.scene.layout.GridPane;
 public class TwoColumnPane extends GridPane 
 {
 	private final int[] paddingData = {5, 5, 5, 15};
+	private ArrayList<Node> nodes;
+	private final int NODES_PER_ROW = 2;
 
 	public TwoColumnPane()
 	{
@@ -16,16 +20,25 @@ public class TwoColumnPane extends GridPane
 		this.setVgap(10);
 
 		this.setPadding(new Insets(paddingData[0], paddingData[1], paddingData[2], paddingData[3]));
+		
+		nodes = new ArrayList<Node>();
 	}
 	
-	protected void addNodes(Node[] nodes)
+	public void addNode(Node node)
 	{
-		for (int c = 0, r= 0, i = 0; i < nodes.length; ++i)
+		nodes.add(node);
+		rebuildPane();
+	}
+	
+	private void rebuildPane()
+	{
+		this.getChildren().clear();
+		for (int c = 0, r= 0, i = 0; i < nodes.size(); ++i)
 		{
-			this.add(nodes[i], c, r);
+			this.add(nodes.get(i), c, r);
 
 			// Increment row and column indexes
-			if (c < (1))
+			if (c < NODES_PER_ROW - 1)
 			{
 				++c;
 			}
