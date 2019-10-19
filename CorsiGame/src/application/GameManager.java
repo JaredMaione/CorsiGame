bec56b9090a373809c9655abbd655820995f1ff2
JaredMaione <jaredm866@gmail.com>
2019-10-19 12:19:33 -0600

    Added other messages for incorrect or game over

--- CorsiGame/src/application/GameManager.java
+++ CorsiGame/src/application/GameManager.java
@@ -16,6 +16,9 @@ public class GameManager
 	private Button submitButton;
 	private final String SUBMIT_BUTTON_LABEL = "Submit";
 	private final String START_MESSAGE_TEXT = "START";
+	private final String CORRECT_MESSAGE_TEXT = "CORRECT";
+	private final String INCORRECT_MESSAGE_TEXT = "INCORRECT";
+	private final String GAME_OVER_MESSAGE_TEXT = "GAME\nOVER";
 	
 	private ArrayList<CorsiBlock> blocks;
 	private ArrayList<CorsiBlock> clickedBlocks;
@@ -40,6 +43,11 @@ public class GameManager
 	
 	private Stage stage;
 	
+	private Text startMessage;
+	private Text correctMessage;
+	private Text incorrectMessage;
+	private Text gameOverMessage;
+	
 	public GameManager(PlayerData playerData, Stage stage)
 	{
 		gameObjects = new Group();
@@ -51,7 +59,35 @@ public class GameManager
 		this.playerData = playerData;
 		
 		gameObjects.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
+		
+		startMessage = new Text(START_MESSAGE_TEXT);
+		startMessage.setLayoutX((gameObjects.getScene().getWidth() / 2) - 130);
+		startMessage.setLayoutY(gameObjects.getScene().getHeight() / 2);
+		startMessage.setVisible(false);
+		startMessage.getStyleClass().add("large_positive_message_text");
+		gameObjects.getChildren().add(startMessage);
+		
+		correctMessage = new Text(CORRECT_MESSAGE_TEXT);
+		correctMessage.setLayoutX((gameObjects.getScene().getWidth() / 2) - 150);
+		correctMessage.setLayoutY(gameObjects.getScene().getHeight() / 2);
+		correctMessage.setVisible(false);
+		correctMessage.getStyleClass().add("large_positive_message_text");
+		gameObjects.getChildren().add(correctMessage);
+		
+		incorrectMessage = new Text(INCORRECT_MESSAGE_TEXT);
+		incorrectMessage.setLayoutX((gameObjects.getScene().getWidth() / 2) - 160);
+		incorrectMessage.setLayoutY(gameObjects.getScene().getHeight() / 2);
+		incorrectMessage.setVisible(false);
+		incorrectMessage.getStyleClass().add("large_negative_message_text");
+		gameObjects.getChildren().add(incorrectMessage);
+		
+		gameOverMessage = new Text(GAME_OVER_MESSAGE_TEXT);
+		gameOverMessage.setLayoutX((gameObjects.getScene().getWidth() / 2) - 130);
+		gameOverMessage.setLayoutY(gameObjects.getScene().getHeight() / 2);
+		gameOverMessage.setVisible(false);
+		gameOverMessage.getStyleClass().add("large_negative_message_text");
+		gameObjects.getChildren().add(gameOverMessage);
+		
 		
 		submitButton = new Button(SUBMIT_BUTTON_LABEL);
 		submitButton.setLayoutX(0);
@@ -135,13 +171,6 @@ public class GameManager
 			gameObjects.getChildren().add(block);
 		}
 		
-		Text startMessage = new Text(START_MESSAGE_TEXT);
-		startMessage.setLayoutX((gameObjects.getScene().getWidth() / 2) - 130);
-		startMessage.setLayoutY(gameObjects.getScene().getHeight() / 2);
-		startMessage.setVisible(false);
-		startMessage.getStyleClass().add("large_message_text");
-		gameObjects.getChildren().add(startMessage);
-		
 		int seconds = sequencePlayer.playSequence(blocks, currentLevel, 1, 1, true);
 		TimedMessageDisplay.displayMessage(startMessage, seconds, 0.2);
 	}
@@ -177,13 +206,19 @@ public class GameManager
 			{
 				++currentLevel;
 				numTries = 0;
+				TimedMessageDisplay.displayMessage(correctMessage, 0, 2);
 				startCurrentLevel();
 			}
 			else
 			{
 				if (numTries < 2)
 				{
+					TimedMessageDisplay.displayMessage(incorrectMessage, 0, 2);
 					startCurrentLevel();
+				}
+				else
+				{
+					TimedMessageDisplay.displayMessage(gameOverMessage, 0, 2);
 				}
 			}
 		}
