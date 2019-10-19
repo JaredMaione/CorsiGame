852d46dfa98f93708dafa36db856834d08bbfb1e
JaredMaione <jaredm866@gmail.com>
2019-10-19 13:35:00 -0600

    Added method getMousePositions()

--- CorsiGame/src/application/GameData.java
+++ CorsiGame/src/application/GameData.java
@@ -2,19 +2,26 @@ package application;
 
 import java.util.ArrayList;
 
-public class GameScore 
+public class GameData 
 {
 	private int corsiSpan;
 	private ElapsedTime avgSequenceTime;
 	private ArrayList<ElapsedTime> sequenceTimes;
 	private ElapsedTime gameDuration;
+	private ArrayList<MousePosition> mousePositions;
 	
-	public GameScore()
+	public GameData()
 	{
 		corsiSpan = 0;
 		sequenceTimes = new ArrayList<ElapsedTime>();
+		mousePositions = new ArrayList<MousePosition>();
 		gameDuration = new ElapsedTime(0);
 		avgSequenceTime = new ElapsedTime(0);
+	}
+	
+	public void addMousePosition(MousePosition pos)
+	{
+		mousePositions.add(pos);
 	}
 	
 	public void addToAvgSequenceTime(ElapsedTime time)
@@ -67,5 +74,10 @@ public class GameScore
 	public void setGameDuration(ElapsedTime duration)
 	{
 		gameDuration = duration;
+	}
+	
+	public ArrayList<MousePosition> getMousePositions()
+	{
+		return mousePositions;
 	}
 }
