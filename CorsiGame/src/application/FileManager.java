8b4dc56c49172d69c5517eb0f91ef77c4cf9c482
JaredMaione <jaredm866@gmail.com>
2019-10-22 18:06:27 -0600

    Began working on file IO

--- CorsiGame/src/application/FileManager.java
+++ CorsiGame/src/application/FileManager.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -12,15 +15,44 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class FileManager 
 {
+	private final String ENCRYPTION_KEY = "123456781234";
+	
 	public FileManager()
 	{
 		
-		String encrypted = encrypt("i am the senate", "123456781234");
-		String decrypted = decrypt(encrypted, "123456781234");
+		String encrypted = encrypt("i am the senate", ENCRYPTION_KEY);
+		String decrypted = decrypt(encrypted, ENCRYPTION_KEY);
 		
 		System.out.println(encrypted);
 		System.out.println(decrypted);
+		
+		PlayerData testData = new PlayerData("user", "pass", new Date(1,1,01), "city", "state", "country", "diagnosis");
+		writeEncrypted(testData.sendToString(), System.getProperty("user.dir") + "\\file.txt");
 	}
+	
+	public void writeEncrypted(String data, String path)
+	{
+		try 
+		{
+			File dataFile = new File(path);
+			FileWriter fileWriter = new FileWriter(dataFile);
+			fileWriter.write(encrypt(path, ENCRYPTION_KEY));
+			
+			fileWriter.close();
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String decryptAndRead(String path)
+	{
+		File dataFile = new File(path);
+		FileReader 
+	}
+	
 	
 	private String encrypt(String message, String key)
 	{
@@ -39,13 +71,13 @@ public class FileManager
 		return "";
 	}
 	
-	private String decrypt(String message, String key)
+	private String decrypt(String encryptedMsg, String key)
 	{
 		try 
 		{
 			Cipher cipher = Cipher.getInstance("AES");
 			cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64.getEncoder().encode(key.getBytes()), "AES"));
-			byte[] bytes = cipher.doFinal(message.getBytes());
+			byte[] bytes = cipher.doFinal(encryptedMsg.getBytes());
 			return new String(bytes);
 		} 
 		catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) 
