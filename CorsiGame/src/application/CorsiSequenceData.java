78bdaba4794c1586dc0122c54fb7f3f603c4051d
JaredMaione <jaredm866@gmail.com>
2019-10-23 19:15:52 -0600

    Added CorsiSequenceData
    This class will eventually be passed to CorsiSequencePlayer.playSequence
    
    It will make it easier to preserve this information

---
+++ CorsiGame/src/application/CorsiSequenceData.java
@@ -0,0 +1,83 @@
+package application;
+
+import java.util.ArrayList;
+
+public class CorsiSequenceData 
+{
+	private ArrayList<CorsiBlock> blocks;
+	private int level;
+	private int secBetweenBlinks;
+	private int blinkSeconds;
+	private boolean startSequenceTimer;
+	private double secToDelay;
+	
+	public CorsiSequenceData(ArrayList<CorsiBlock> blocks, int level, int secBetweenBlinks, int blinkSeconds, boolean startSequenceTimer, double secToDelay)
+	{
+		this.blocks = blocks;
+		this.level = level;
+		this.secBetweenBlinks = secBetweenBlinks;
+		this.blinkSeconds = blinkSeconds;
+		this.startSequenceTimer = startSequenceTimer;
+		this.secToDelay = secToDelay;
+	}
+
+	public ArrayList<CorsiBlock> getBlocks() 
+	{
+		return blocks;
+	}
+
+	public void setBlocks(ArrayList<CorsiBlock> blocks) 
+	{
+		this.blocks = blocks;
+	}
+
+	public int getLevel() 
+	{
+		return level;
+	}
+
+	public void setLevel(int level) 
+	{
+		this.level = level;
+	}
+
+	public int getSecBetweenBlinks() 
+	{
+		return secBetweenBlinks;
+	}
+
+	public void setSecBetweenBlinks(int secBetweenBlinks) 
+	{
+		this.secBetweenBlinks = secBetweenBlinks;
+	}
+
+	public int getBlinkSeconds() 
+	{
+		return blinkSeconds;
+	}
+
+	public void setBlinkSeconds(int blinkSeconds) 
+	{
+		this.blinkSeconds = blinkSeconds;
+	}
+
+	public boolean isStartSequenceTimer() 
+	{
+		return startSequenceTimer;
+	}
+
+	public void setStartSequenceTimer(boolean startSequenceTimer) 
+	{
+		this.startSequenceTimer = startSequenceTimer;
+	}
+
+	public double getSecToDelay() 
+	{
+		return secToDelay;
+	}
+
+	public void setSecToDelay(double secToDelay) 
+	{
+		this.secToDelay = secToDelay;
+	}
+}
