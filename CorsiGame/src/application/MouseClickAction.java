38fd4119214008e18f171c09cc3003006eddaeaa
JaredMaione <jaredm866@gmail.com>
2019-10-24 11:06:46 -0600

    Removed drag parameter from other constructor

--- CorsiGame/src/application/MouseClickAction.java
+++ CorsiGame/src/application/MouseClickAction.java
@@ -3,20 +3,17 @@ package application;
 public class MouseClickAction extends MouseAction
 {
 	private int clickCount;
-	private boolean dragged;
 	
 	public MouseClickAction(long msFromStart, Position mousePosition, int clickCount)
 	{
 		super(msFromStart, mousePosition);
 		this.clickCount = clickCount;
-		this.dragged = dragged;
 	}
 	
-	public MouseClickAction(long startTime, long currentTime, Position mousePosition, int clickCount, boolean dragged)
+	public MouseClickAction(long startTime, long currentTime, Position mousePosition, int clickCount)
 	{
 		super(startTime, currentTime, mousePosition);
 		
 		this.clickCount = clickCount;
-		this.dragged = dragged;
 	}
 }
