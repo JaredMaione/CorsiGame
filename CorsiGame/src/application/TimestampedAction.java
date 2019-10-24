9845f1b6a56117a63431916bcf6297bae9038e58
JaredMaione <jaredm866@gmail.com>
2019-10-23 19:05:05 -0600

    Added members and constructors

--- CorsiGame/src/application/TimestampedAction.java
+++ CorsiGame/src/application/TimestampedAction.java
@@ -1,5 +1,17 @@
 package application;
 
-public class TimestampedAction {
-
+public class TimestampedAction 
+{
+	protected long msFromStart;
+	
+	public TimestampedAction(long msFromStart)
+	{
+		this.msFromStart = msFromStart;
+	}
+	
+	// This constructor calculates msFromStart (calculation is abstracted away)
+	public TimestampedAction(long startTime, long currentTime)
+	{
+		msFromStart = currentTime - startTime;
+	}
 }
