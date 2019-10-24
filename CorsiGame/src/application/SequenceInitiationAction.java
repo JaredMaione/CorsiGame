3da2c49cf74605450a761bf2506fc6426e151330
JaredMaione <jaredm866@gmail.com>
2019-10-23 19:18:01 -0600

    Added member sequenceData along with getter

--- CorsiGame/src/application/SequenceInitiationAction.java
+++ CorsiGame/src/application/SequenceInitiationAction.java
@@ -1,5 +1,25 @@
 package application;
 
-public class SequenceInitiationAction {
+import java.util.ArrayList;
 
+public class SequenceInitiationAction extends TimestampedAction
+{
+	private CorsiSequenceData sequenceData;
+	
+	public SequenceInitiationAction(long msFromStart, CorsiSequenceData sequenceData)
+	{
+		super(msFromStart);
+		this.sequenceData = sequenceData;
+	}
+	
+	public SequenceInitiationAction(long startTime, long currentTime, CorsiSequenceData sequenceData)
+	{
+		super(startTime, currentTime);
+		this.sequenceData = sequenceData;
+	}
+	
+	public CorsiSequenceData getSequence()
+	{
+		return sequenceData;
+	}
 }
