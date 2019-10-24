617a154222c81f390c3f99160ca11f5aca19f5a8
JaredMaione <jaredm866@gmail.com>
2019-10-23 19:37:59 -0600

    Changed to inherit from MouseClickedAction

--- CorsiGame/src/application/BlockClickedAction.java
+++ CorsiGame/src/application/BlockClickedAction.java
@@ -1,18 +1,18 @@
 package application;
 
-public class BlockClickedAction extends MouseAction
+public class BlockClickedAction extends MouseClickAction
 {
 	private CorsiBlock clickedBlock;
 	
-	public BlockClickedAction(long msFromStart, Position clickPosition, CorsiBlock clickedBlock)
+	public BlockClickedAction(long msFromStart, Position mousePosition, int clickCount, boolean dragged, CorsiBlock clickedBlock)
 	{
-		super(msFromStart, clickPosition);
+		super(msFromStart, mousePosition, clickCount, dragged);
 		this.clickedBlock = clickedBlock;
 	}
 	
-	public BlockClickedAction(long startTime, long currentTime, Position clickPosition, CorsiBlock clickedBlock)
+	public BlockClickedAction(long startTime, long currentTime, Position mousePosition,  int clickCount, boolean dragged, CorsiBlock clickedBlock)
 	{
-		super(startTime, currentTime, clickPosition);
+		super(startTime, currentTime, mousePosition, clickCount, dragged);
 		this.clickedBlock = clickedBlock;
 	}
 	
