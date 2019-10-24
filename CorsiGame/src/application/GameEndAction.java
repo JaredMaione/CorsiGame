dd6f2df01293fef09ee6e5a2ed52e914efea3194
JaredMaione <jaredm866@gmail.com>
2019-10-23 19:10:57 -0600

    Added constructors and set up inheritance

--- CorsiGame/src/application/GameEndAction.java
+++ CorsiGame/src/application/GameEndAction.java
@@ -1,5 +1,14 @@
 package application;
 
-public class GameEndAction {
-
+public class GameEndAction extends TimestampedAction 
+{
+	public GameEndAction(long msFromStart)
+	{
+		super(msFromStart);
+	}
+	
+	public GameEndAction(long startTime, long currentTime)
+	{
+		super(startTime, currentTime);
+	}
 }
