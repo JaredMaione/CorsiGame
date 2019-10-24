5ab5dbc465964a889fc4488311403bfe7bff9d28
JaredMaione <jaredm866@gmail.com>
2019-10-24 11:06:59 -0600

    Removed drag detection

--- CorsiGame/src/application/BlockClickedAction.java
+++ CorsiGame/src/application/BlockClickedAction.java
@@ -4,15 +4,15 @@ public class BlockClickedAction extends MouseClickAction
 {
 	private CorsiBlock clickedBlock;
 	
-	public BlockClickedAction(long msFromStart, Position mousePosition, int clickCount, boolean dragged, CorsiBlock clickedBlock)
+	public BlockClickedAction(long msFromStart, Position mousePosition, int clickCount, CorsiBlock clickedBlock)
 	{
-		super(msFromStart, mousePosition, clickCount, dragged);
+		super(msFromStart, mousePosition, clickCount);
 		this.clickedBlock = clickedBlock;
 	}
 	
-	public BlockClickedAction(long startTime, long currentTime, Position mousePosition,  int clickCount, boolean dragged, CorsiBlock clickedBlock)
+	public BlockClickedAction(long startTime, long currentTime, Position mousePosition,  int clickCount, CorsiBlock clickedBlock)
 	{
-		super(startTime, currentTime, mousePosition, clickCount, dragged);
+		super(startTime, currentTime, mousePosition, clickCount);
 		this.clickedBlock = clickedBlock;
 	}
 	
