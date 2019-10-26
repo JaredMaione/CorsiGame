b8505b073c5105ebfa3f43052028c8f529365dfd
JaredMaione <jaredm866@gmail.com>
2019-10-26 11:04:21 -0600

    Added generated serialization ID

--- CorsiGame/src/application/MouseAction.java
+++ CorsiGame/src/application/MouseAction.java
@@ -5,7 +5,7 @@ public class MouseAction extends TimestampedAction
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1731036758250251218L;
 	
 	protected Position mousePosition;
 	
