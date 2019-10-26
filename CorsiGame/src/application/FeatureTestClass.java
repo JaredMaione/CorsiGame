172759c15a86013200f1157e1a17543e26c3a28b
JaredMaione <jaredm866@gmail.com>
2019-10-25 20:37:42 -0600

    Testing serialization

--- CorsiGame/src/application/FeatureTestClass.java
+++ CorsiGame/src/application/FeatureTestClass.java
@@ -2,6 +2,8 @@ package application;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class FeatureTestClass implements Serializable
@@ -11,17 +13,13 @@ public class FeatureTestClass implements Serializable
 	 * 
 	 */
 	private static final long serialVersionUID = 12L;
-	private int x;
-	private int y;
+	public int x;
+	public int y;
 	
 	public FeatureTestClass()
 	{
-		try {
-			FileOutputStream os = new FileOutputStream("test.ser");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		x = 5;
+		y = -1;
 	}
 	
 }
--- CorsiGame/src/application/Main.java
+++ CorsiGame/src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 	
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import javafx.animation.AnimationTimer;
@@ -22,9 +26,26 @@ public class Main extends Application {
 			//GameManager manny = new GameManager(new PlayerData(), primaryStage);
 			//mainMenu menu = new MainMenu(primaryStage);
 			//RegistrationMenu reggie = new RegistrationMenu(primaryStage);
+			
+			//PlayerData data = new PlayerData("User", "Pass", new Date(1,1,19), "City", "State", "Zip", "Diagnosis");
+			//PlayerData data = new PlayerData();
 			//FileManager fm = new FileManager();
+			//fm.writeEncrypted(data.sendToString(), System.getProperty("user.dir") + "\\kung.foo");
+			//data.readFromString(fm.decryptAndRead(System.getProperty("user.dir") + "\\kung.foo"));
 			//ReturningPlayerMenu menu = new ReturningPlayerMenu(primaryStage);
 			
+			
+			FileOutputStream os = new FileOutputStream("test.ser");
+			ObjectOutputStream objStream = new ObjectOutputStream(os);
+			objStream.writeObject(new FeatureTestClass());
+			objStream.close();
+
+			ObjectInputStream objInStream = new ObjectInputStream(new FileInputStream("test.ser"));
+			
+			FeatureTestClass foo = (FeatureTestClass) objInStream.readObject();
+			System.out.println(foo.x);
+			System.out.println(foo.y);
+			objInStream.close();
 
 		} catch(Exception e) {
 			e.printStackTrace();
