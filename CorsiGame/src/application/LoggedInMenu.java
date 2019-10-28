4569295b87ae240300c5c6c030a9609656e07ef1
JaredMaione <jaredm866@gmail.com>
2019-10-27 19:32:51 -0600

    Added some members

--- CorsiGame/src/application/LoggedInMenu.java
+++ CorsiGame/src/application/LoggedInMenu.java
@@ -1,5 +1,17 @@
 package application;
 
-public class LoggedInMenu {
+import java.util.ArrayList;
 
+import javafx.stage.Stage;
+
+public class LoggedInMenu 
+{
+	private ArrayList<PlayerData> players;
+	private Stage stage;
+	
+	public LoggedInMenu(Stage stage, ArrayList<PlayerData> players)
+	{
+		this.stage = stage;
+		this.players = players;
+	}
 }
