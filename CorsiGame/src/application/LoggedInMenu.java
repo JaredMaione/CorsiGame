c7043221c3f8d5f4591e083e8c05fdcedd363db0
JaredMaione <jaredm866@gmail.com>
2019-10-29 12:32:39 -0600

    Added member variables

--- CorsiGame/src/application/LoggedInMenu.java
+++ CorsiGame/src/application/LoggedInMenu.java
@@ -2,14 +2,25 @@ package application;
 
 import java.util.ArrayList;
 
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class LoggedInMenu 
 {
+	private final String PLAY_BUTTON_LABEL = "Play Game";
+	private final String SCOREBOARD_BUTTON_LABEL = "View Scoreboard";
+	private final String LOGOUT_BUTTON_LABEL = "Logout & Return to Menu";
+
 	private ArrayList<PlayerData> players;
+	private PlayerData loggedInPlayer;
+	
 	private Stage stage;
 	
-	public LoggedInMenu(Stage stage, ArrayList<PlayerData> players)
+	private Button playButton;
+	private Button logoutButton;
+	private Button scoreboardButton;
+	
+	public LoggedInMenu(Stage stage, PlayerData loggedInPlayer, ArrayList<PlayerData> players)
 	{
 		this.stage = stage;
 		this.players = players;
