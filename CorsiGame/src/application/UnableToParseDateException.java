3c1c2507bd66061c2f0f2ccf5cfc918589ee10cc
JaredMaione <jaredm866@gmail.com>
2019-10-30 16:34:42 -0600

    Added serialization ID

--- CorsiGame/src/application/UnableToParseDateException.java
+++ CorsiGame/src/application/UnableToParseDateException.java
@@ -2,6 +2,11 @@ package application;
 
 public class UnableToParseDateException extends Exception
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4427133837310484723L;
+
 	public UnableToParseDateException(String s)
 	{
 		super(s);
