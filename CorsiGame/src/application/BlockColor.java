d5e5ea087370a96aa9db332bca7499c9ee429037
JaredMaione <jaredm866@gmail.com>
2019-10-30 16:45:39 -0600

    Added enum to keep track of block colors and be Serializable

---
+++ CorsiGame/src/application/BlockColor.java
@@ -0,0 +1,8 @@
+package application;
+
+// The purpose of this class is to provide a way to keep track of Corsi block colors without attempting to
+// serialize the non-serializable Color class
+public enum BlockColor 
+{
+	RED, BLUE;
+}
