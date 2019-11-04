648b44dd51f6a1348975f0614672831226c284d3
JaredMaione <jaredm866@gmail.com>
2019-11-03 19:28:18 -0700

    Added serialization ID

--- CorsiGame/src/application/BlockClickedAction.java
+++ CorsiGame/src/application/BlockClickedAction.java
@@ -2,6 +2,8 @@ package application;
 
 public class BlockClickedAction extends MouseClickAction
 {
+	private static final long serialVersionUID = -3919053914713999810L;
+	
 	private CorsiBlock clickedBlock;
 	
 	public BlockClickedAction(long msFromStart, Position mousePosition, int clickCount, CorsiBlock clickedBlock)
