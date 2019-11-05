e33c3b4cb02712ea5e2f81437fecc2f23c61a830
JaredMaione <jaredm866@gmail.com>
2019-11-05 13:17:49 -0700

    Added class
    This class will allow the admin to open a player's stats

---
+++ CorsiGame/src/application/PlayerViewMenu.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.stage.Stage;
+
+public class PlayerViewMenu 
+{
+	private Stage stage;
+	private PlayerData player;
+	
+	public PlayerViewMenu(Stage stage, PlayerData player)
+	{
+		this.stage = stage;
+		this.player = player;
+	}
+}
