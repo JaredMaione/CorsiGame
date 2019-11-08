36e0c3bec39161c3acade68c66cd61b56426e236
JaredMaione <jaredm866@gmail.com>
2019-11-07 17:39:17 -0700

    Added class which will be responsible for game replay

---
+++ CorsiGame/src/application/GameReplayManager.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.ArrayList;
+
+import javafx.stage.Stage;
+
+public class GameReplayManager extends GameManager
+{
+	public GameReplayManager(Stage stage, GameData game, ArrayList<PlayerData> players)
+	{
+		super(stage);
+	}
+}
