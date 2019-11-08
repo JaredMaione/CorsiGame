c0fe0aea0afefd97500593df9a9d7544f9176359
JaredMaione <jaredm866@gmail.com>
2019-11-07 19:25:57 -0700

    Fixed logic error with event time condition

--- CorsiGame/src/application/GameReplayManager.java
+++ CorsiGame/src/application/GameReplayManager.java
@@ -9,57 +9,57 @@ import javafx.stage.Stage;
 public class GameReplayManager extends GameManager
 {
 	private final int CURSOR_RADIUS = 4;
-	
+
 	private ArrayList<TimestampedAction> actionQueue;
+	private ArrayList<TimestampedAction> actionBuffer;
 	private int actionIndex;
 	private Stopwatch replayStopwatch;
 	private Circle cursor;
-	
+
 	public GameReplayManager(Stage stage, GameData gameData, PlayerData playerData, ArrayList<PlayerData> players)
 	{
 		super(stage);
 		this.playerData = playerData;
 		this.players = players;
 		this.gameData = gameData;
-		
+
 		actionQueue = gameData.getGameActions();
+		actionBuffer = new ArrayList<TimestampedAction>();
 		actionIndex = 0;
 		replayStopwatch = new Stopwatch();
 		cursor = new Circle(CURSOR_RADIUS);
-		
+
 		gameObjects.getChildren().add(cursor);
-		
+
 		beginSimulation();
 	}
-	
+
 	private void beginSimulation()
 	{
 		replayStopwatch.start();
-		System.out.println(actionQueue.size());
-		
+
 		AnimationTimer gameTimer = new AnimationTimer()
 		{
 			@Override
 			public void handle(long arg0) 
 			{
-				System.out.println(actionQueue.get(actionIndex).getMSFromStart());
-				if (actionQueue.get(actionIndex).getMSFromStart() >= replayStopwatch.getMSFromStart())
+
+				if (replayStopwatch.getMSFromStart() >= actionQueue.get(actionIndex).getMSFromStart())
 				{
+
 					TimestampedAction currentAction = actionQueue.get(actionIndex);
-					
+
 					if (currentAction instanceof BlockClickedAction)
 					{
 						System.out.println("BlockAction");
 					}
 					else if (currentAction instanceof MouseClickAction)
 					{
-						cursor.setCenterX(((MouseClickAction) currentAction).getMousePosition().getX());
-						cursor.setCenterY(((MouseClickAction) currentAction).getMousePosition().getY());
-						System.out.println(((MouseClickAction) currentAction).getMousePosition().getX());
+						System.out.println("ClickAction");
 					}
 					else if (currentAction instanceof MouseAction)
 					{
-						System.out.println("MouseAction");
+						System.out.println("Mouse moved to " + ((MouseAction) currentAction).getMousePosition().getX() + " " + ((MouseAction) currentAction).getMousePosition().getY());
 					}
 					else if (currentAction instanceof SequenceInitiationAction)
 					{
@@ -73,13 +73,14 @@ public class GameReplayManager extends GameManager
 					{
 						System.out.println("GameEnd");
 					}
-					
+
 					++actionIndex;
 				}
+
 			}
 
 		};
-		
+
 		gameTimer.start();
 	}
 }
