d91e7486923724c26e113c453ad9902d7197c87d
JaredMaione <jaredm866@gmail.com>
2019-11-07 17:52:02 -0700

    Began developing simulation class

--- CorsiGame/src/application/GameReplayManager.java
+++ CorsiGame/src/application/GameReplayManager.java
@@ -2,12 +2,69 @@ package application;
 
 import java.util.ArrayList;
 
+import javafx.animation.AnimationTimer;
 import javafx.stage.Stage;
 
 public class GameReplayManager extends GameManager
 {
-	public GameReplayManager(Stage stage, GameData game, ArrayList<PlayerData> players)
+	private ArrayList<TimestampedAction> actionQueue;
+	private int actionIndex;
+	private Stopwatch replayStopwatch;
+	
+	public GameReplayManager(Stage stage, GameData gameData, PlayerData playerData, ArrayList<PlayerData> players)
 	{
 		super(stage);
+		this.playerData = playerData;
+		this.players = players;
+		this.gameData = gameData;
+		actionQueue = gameData.getGameActions();
+		actionIndex = 0;
+		replayStopwatch = new Stopwatch();
+		beginSimulation();
+	}
+	
+	private void beginSimulation()
+	{
+		replayStopwatch.start();
+		
+		AnimationTimer gameTimer = new AnimationTimer()
+		{
+			@Override
+			public void handle(long arg0) 
+			{
+				if (actionQueue.get(actionIndex).getMSFromStart() >= replayStopwatch.getMSFromStart())
+				{
+					TimestampedAction currentAction = actionQueue.get(actionIndex);
+					
+					if (currentAction instanceof BlockClickedAction)
+					{
+						
+					}
+					else if (currentAction instanceof MouseClickAction)
+					{
+						
+					}
+					else if (currentAction instanceof MouseAction)
+					{
+						
+					}
+					else if (currentAction instanceof SequenceInitiationAction)
+					{
+					
+					}
+					else if (currentAction instanceof SubmitClickedAction)
+					{
+						
+					}
+					else if (currentAction instanceof GameEndAction)
+					{
+						
+					}
+				}
+			}
+
+		};
+		
+		gameTimer.start();
 	}
 }
