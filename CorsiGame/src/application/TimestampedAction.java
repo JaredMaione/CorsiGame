d4b5402a09ad315ae5e7ce4b33ea2e6fe704f8e9
JaredMaione <jaredm866@gmail.com>
2019-11-12 11:30:16 -0700

    Changed msFromStart to private

--- CorsiGame/src/application/TimestampedAction.java
+++ CorsiGame/src/application/TimestampedAction.java
@@ -7,7 +7,7 @@ public abstract class TimestampedAction implements Serializable
 	private static final long serialVersionUID = -3686299188586827656L;
 	
 	public static final String FILE_IDENTIFIER = "TIMESTAMPED_ACTION";
-	protected long msFromStart;
+	private long msFromStart;
 	
 	public TimestampedAction(long msFromStart)
 	{
