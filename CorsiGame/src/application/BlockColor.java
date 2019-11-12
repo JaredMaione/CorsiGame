482399e2180dd9a56714f4a2ba00d5380e5c193f
JaredMaione <jaredm866@gmail.com>
2019-11-12 11:39:47 -0700

    Modified comment

--- CorsiGame/src/application/BlockColor.java
+++ CorsiGame/src/application/BlockColor.java
@@ -1,6 +1,6 @@
 package application;
 
-// The purpose of this class is to provide a way to keep track of Corsi block colors without attempting to
+// This class provides a way to keep track of Corsi block colors without attempting to
 // serialize the non-serializable Color class
 public enum BlockColor 
 {
