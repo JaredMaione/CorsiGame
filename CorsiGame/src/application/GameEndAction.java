a6b85436c9c4b35366aceb37dcb096596e0a8db6
JaredMaione <jaredm866@gmail.com>
2019-11-12 11:35:09 -0700

    Removed auto generated comment

--- CorsiGame/src/application/GameEndAction.java
+++ CorsiGame/src/application/GameEndAction.java
@@ -2,9 +2,6 @@ package application;
 
 public class GameEndAction extends TimestampedAction 
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -237313697040482769L;
 
 	public GameEndAction(long msFromStart)
--- CorsiGame/src/application/UnableToParseDateException.java
+++ CorsiGame/src/application/UnableToParseDateException.java
@@ -2,9 +2,6 @@ package application;
 
 public class UnableToParseDateException extends Exception
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4427133837310484723L;
 
 	public UnableToParseDateException(String s)
