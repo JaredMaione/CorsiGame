4528c50b8949f4b5abe68bad096e8e1c218981c5
JaredMaione <jaredm866@gmail.com>
2019-11-17 16:16:52 -0700

    Added scroll bar

--- CorsiGame/src/application/HelpMenu.java
+++ CorsiGame/src/application/HelpMenu.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
@@ -22,12 +23,13 @@ public class HelpMenu
 	
 	private final int[] paddingData = {5, 5, 10, 15};
 	
-	private final int WINDOW_WIDTH = 500;
+	private final int WINDOW_WIDTH = 600;
 	private final int WINDOW_HEIGHT = 400;
 	
 	private Stage stage;
 	private Button menuButton;
 	private FlowPane mainPane;
+	private ScrollPane scrollPane;
 	
 	public HelpMenu(Stage stage)
 	{
@@ -35,6 +37,8 @@ public class HelpMenu
 		
 		mainPane = new FlowPane(Orientation.VERTICAL);
 		mainPane.setPadding(new Insets(paddingData[0], paddingData[1], paddingData[2], paddingData[3]));
+		
+		scrollPane = new ScrollPane();
 		
 		menuButton = new Button(MENU_BUTTON_TEXT);
 		
@@ -61,16 +65,18 @@ public class HelpMenu
 			{
 				helpFile.createNewFile();
 			} 
-			catch (IOException e1) 
+			catch (IOException e) 
 			{
-				e1.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 		
 		Text helpText = new Text(FileManager.readTextFile(helpFile));
 		helpText.setWrappingWidth(WINDOW_WIDTH - 50);
+		scrollPane.setContent(helpText);
+		scrollPane.setPrefSize(WINDOW_WIDTH - 25, WINDOW_HEIGHT - 75);
 		
-		mainPane.getChildren().add(helpText);
+		mainPane.getChildren().add(scrollPane);
 		mainPane.getChildren().add(menuButton);
 		
 		stage.setScene(new Scene(mainPane, WINDOW_WIDTH, WINDOW_HEIGHT));
