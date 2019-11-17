32965cf57e7f17ec452ed6988882abf1b81d4a9a
JaredMaione <jaredm866@gmail.com>
2019-11-17 15:08:04 -0700

    Added comment

--- CorsiGame/src/application/SubmitClickedAction.java
+++ CorsiGame/src/application/SubmitClickedAction.java
@@ -1,5 +1,6 @@
 package application;
 
+// Keeps track of when the submit button was clicked after a sequence
 public class SubmitClickedAction extends MouseClickAction 
 {
 	private static final long serialVersionUID = -5774668168222264880L;
