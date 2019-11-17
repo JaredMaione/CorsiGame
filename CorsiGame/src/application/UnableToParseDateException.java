3a64176c2b30afb5e926ff758d6acd77838fb7ce
JaredMaione <jaredm866@gmail.com>
2019-11-17 15:15:36 -0700

    Added class description

--- CorsiGame/src/application/UnableToParseDateException.java
+++ CorsiGame/src/application/UnableToParseDateException.java
@@ -1,5 +1,6 @@
 package application;
 
+// This exception is thrown if an argument String is unable to be parsed in a Date constructor
 public class UnableToParseDateException extends Exception
 {
 	private static final long serialVersionUID = 4427133837310484723L;
