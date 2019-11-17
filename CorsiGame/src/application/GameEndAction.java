d9c7de8a3b58754cc5b08b8ccb2ff4f626820374
JaredMaione <jaredm866@gmail.com>
2019-11-17 10:31:12 -0700

    Added description comment

--- CorsiGame/src/application/GameEndAction.java
+++ CorsiGame/src/application/GameEndAction.java
@@ -1,5 +1,6 @@
 package application;
 
+// This class keeps track of the time at which a game ended
 public class GameEndAction extends TimestampedAction 
 {
 	private static final long serialVersionUID = -237313697040482769L;
