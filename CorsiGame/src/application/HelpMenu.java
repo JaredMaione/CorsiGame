e5d5117364529e11dacca24e09dacdec7e81cda5
JaredMaione <jaredm866@gmail.com>
2019-11-17 15:47:27 -0700

    Added class

---
+++ CorsiGame/src/application/HelpMenu.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.stage.Stage;
+
+// This class is responsible for displaying the help menu
+// It can transition back to the main menu when the player chooses to do so
+public class HelpMenu 
+{
+	public HelpMenu(Stage stage)
+	{
+		
+	}
+}
